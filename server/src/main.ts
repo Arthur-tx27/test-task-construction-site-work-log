39fab4d872c9361d3e7c0c9696f699345533a79b
Arthur <arturka_tx@mail.ru>
2026-05-25 15:54:49 +0300

    feat: настройка CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
